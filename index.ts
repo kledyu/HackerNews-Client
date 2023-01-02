f6b790f404d95c39857aab07317b7f0086d1804a
Kled <1017yu@daum.net>
2023-01-02 17:26:56 +0900

    Initial commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+type Centimeter = number;
+type RainbowColor = "red" | "orange" | "yellow" | "green";
+
+let age = 10; // js
+let weight: number = 70; // ts
+let height: Centimeter = 185;
+
+let color: RainbowColor = "orange";
